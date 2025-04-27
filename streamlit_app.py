39769f0cd0fd3e6d4ff36e8af41415c419511bd2
tomomi-git <stomomi12110403@gmail.com>
2025-04-27 10:51:18 +0000

    henko

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,6 +5,8 @@ st.title("🎈 テスト（画像表示アプリ）")
 
 uploaded_file = st.file_uploader("画像を選択してください", type=["png", "jpg", "jpeg"])
 
+image = Image.open(uploaded_file)
+    
 max_size = (300, 300)
 image.thumbnail(max_size)
 original_width, original_height = image.size
