fcc9881386e7737c519995459257c1f40b2335bd
tomomi-git <stomomi12110403@gmail.com>
2025-04-27 11:04:26 +0000

    henko3

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,8 +4,3 @@ from PIL import Image
 st.title("🎈 テスト（画像表示アプリ）")
 
 uploaded_file = st.file_uploader("画像を選択してください", type=["png", "jpg", "jpeg"])
-
-image = Image.open(uploaded_file)    
-max_size = (300, 300)
-image.thumbnail(max_size)
-original_width, original_height = image.size
